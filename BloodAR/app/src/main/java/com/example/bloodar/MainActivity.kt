ae0188217f032015af3c027edad7b99b57b1d9b1
Knurkie <lucasimonetti15@live.nl>
2022-11-08 12:49:31 +0100

    Added new activities for Syndrome list and Feedback button.

--- BloodAR/app/src/main/java/com/example/bloodar/MainActivity.kt
+++ BloodAR/app/src/main/java/com/example/bloodar/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.example.bloodar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val syndromeButton: Button = findViewById(R.id.syndromeButton)
+        syndromeButton.setOnClickListener {openSyndromeListActivity()}
+
+        val feedbackButton: FloatingActionButton = findViewById(R.id.feedbackButton)
+        feedbackButton.setOnClickListener {openFeedbackActivity()}
         }
-    /** Called when the user touches the 'Syndromes' button */
-    fun sendSyndromeMessage(view: View) {
-// Do something in response to button click
-        Toast.makeText(this, "Display list of syndromes", Toast.LENGTH_SHORT).show()
-    }
+
     /** Called when the user touches the 'AR' button */
     fun sendARMessage(view: View) {
-// Do something in response to button click
         Toast.makeText(this, "Open camera and display AR model", Toast.LENGTH_SHORT).show()
     }
     /** Called when the user touches the 'AR' button */
     fun sendFeedbackMessage(view: View) {
-// Do something in response to button click
         Toast.makeText(this, "Open feedback screen1", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openSyndromeListActivity() {
+        val intent = Intent(this, SyndromeListActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun openFeedbackActivity() {
+        val intent = Intent(this, FeedbackActivity::class.java)
+        startActivity(intent)
     }
 }
