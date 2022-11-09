da92d2a949b3fc69ecff5ae1bb71c6e215013457
Knurkie <lucasimonetti15@live.nl>
2022-11-09 16:08:02 +0100

    Added description and image of Atrial Fibrillation.

---
+++ BloodAR/app/src/main/java/com/example/bloodar/AtherosclerosisActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bloodar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AtherosclerosisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atherosclerosis)
+    }
+}
--- BloodAR/app/src/main/java/com/example/bloodar/SyndromeListActivity.kt
+++ BloodAR/app/src/main/java/com/example/bloodar/SyndromeListActivity.kt
@@ -1,11 +1,30 @@
 package com.example.bloodar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SyndromeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_syndrome_list)
+
+        val atrialFibrillationButton: Button = findViewById(R.id.atrialFibrillation)
+        atrialFibrillationButton.setOnClickListener{openAtrialFibrillationActivity()}
+
+        val atherosclerosisButton: Button = findViewById(R.id.atherosclerosis)
+        atherosclerosisButton.setOnClickListener{openAtherosclerosisActivity()}
+
+    }
+
+    private fun openAtrialFibrillationActivity() {
+        val intent = Intent(this, AtrialFibrillationActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun openAtherosclerosisActivity() {
+        val intent = Intent(this, AtherosclerosisActivity::class.java)
+        startActivity(intent)
     }
 }
