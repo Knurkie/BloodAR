85ebf979afa5d4675a506dbabfc794098d9125c4
Knurkie <lucasimonetti15@live.nl>
2022-12-06 12:06:24 +0100

    Removed redundant, commented code. Added more 3D models. Fixed the app crashing when returning to the Main Activity from the AR Activity.

--- BloodAR/app/src/main/java/com/example/bloodar/ArActivity.kt
+++ BloodAR/app/src/main/java/com/example/bloodar/ArActivity.kt
@@ -1,12 +1,13 @@
 package com.example.bloodar
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
@@ -28,13 +29,13 @@ class ArActivity : AppCompatActivity(R.layout.activity_ar) {
         val fileLocation: String,
         val scaleUnits: Float? = null,
         val placementMode: PlacementMode = PlacementMode.BEST_AVAILABLE,
-        val applyPoseRotation: Boolean = true
+        val applyPoseRotation: Boolean = true,
     )
 
     val models = listOf(
         Model("blood_circulation.glb"),
         Model(fileLocation = "beating_heart_intersection.glb"),
-        Model(fileLocation = "cardiac_anatomy_external_view.glb"),
+        Model(fileLocation = "cardiac_anatomy_external_view.glb", scaleUnits = 0.3f),
         Model(fileLocation = "heart.glb")
     )
 
@@ -142,107 +143,18 @@ class ArActivity : AppCompatActivity(R.layout.activity_ar) {
 
 
 
+//    Temporary way to fix the app crashing when returning to the Main Activity from the AR Activity.
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed(){
+//        Toast.makeText(applicationContext, "Back button pressed!", Toast.LENGTH_SHORT).show() // Uncomment for debugging purposes.
+        openMainActivity()
+    }
+
+    private fun openMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
-
-
-
-////import android.R
-//import android.os.Bundle
-//import android.widget.Toast
-//import androidx.appcompat.app.AppCompatActivity
-//import com.example.bloodar.CameraPermissionHelper.checkCameraPermission
-//import com.example.bloodar.CameraPermissionHelper.requestCameraPermission
-//import com.google.ar.core.ArCoreApk
-//import com.google.ar.core.Session
-//import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
-//import com.google.ar.sceneform.ux.ArFragment
-//import io.github.sceneview.ar.ArFragment
-//import kotlinx.android.synthetic.main.activity_ar.*
-//
-//
-//class ArActivity : AppCompatActivity() {
-////    private val PERMISSION_REQUEST_CODE = 200
-//    private lateinit var mSession: Session
-//
-//    private var arFragment: ArFragment? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_ar)
-//
-//        // Load model.glb from assets folder or http url
-//        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment?
-//        arFragment!!.setOnTapPlaneGlbModel("Cappuccino_cup.glb")
-//        //        arFragment.setOnTapPlaneGlbModel("https://github.com/codemaker2015/3d-models/blob/master/glb_files/model.glb?raw=true");
-//    }
-//
-//    // requestInstall(Activity, true) will triggers installation of
-//// Google Play Services for AR if necessary.
-//    private var mUserRequestedInstall = true
-//
-//    override fun onResume() {
-//        super.onResume()
-//
-//        // Check camera permission.
-//            // ARCore requires camera permission to operate.
-//        if (!checkCameraPermission(this)) {
-//            requestCameraPermission(this)
-//            return
-//            }
-//
-//        // Ensure that Google Play Services for AR and ARCore device profile data are
-//        // installed and up to date.
-//        try {
-//            if (mSession == null) {
-//                when (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
-//                    ArCoreApk.InstallStatus.INSTALLED -> {
-//                        // Success: Safe to create the AR session.
-//                        mSession = Session(this)
-//                    }
-//                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
-//                        // When this method returns `INSTALL_REQUESTED`:
-//                        // 1. ARCore pauses this activity.
-//                        // 2. ARCore prompts the user to install or update Google Play
-//                        //    Services for AR (market://details?id=com.google.ar.core).
-//                        // 3. ARCore downloads the latest device profile data.
-//                        // 4. ARCore resumes this activity. The next invocation of
-//                        //    requestInstall() will either return `INSTALLED` or throw an
-//                        //    exception if the installation or update did not succeed.
-//                        mUserRequestedInstall = false
-//                        return
-//                    }
-//                }
-//            }
-//        } catch (e: UnavailableUserDeclinedInstallationException) {
-//            // Display an appropriate message to the user and return gracefully.
-//            Toast.makeText(this, "TODO: handle exception $e", Toast.LENGTH_LONG)
-//                .show()
-//            return
-//        } catch (e: Throwable) {
-//
-//            return  // mSession remains null, since session creation has failed.
-//        }
-//
-//    }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        results: IntArray,
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, results)
-//        if (!checkCameraPermission(this)) {
-//            Toast.makeText(
-//                this,
-//                "Camera permission is needed to run this application",
-//                Toast.LENGTH_LONG
-//            )
-//                .show()
-//            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-//                // Permission denied with checking "Do not ask again".
-//                CameraPermissionHelper.launchPermissionSettings(this)
-//            }
-//            finish()
-//        }
-//    }
-//}
